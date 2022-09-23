f4aa54004f6e58d325372e47528ce8505417e54d
tereesaalvarez <alvdeporteresa@gmail.com>
2022-09-23 17:39:19 +0200

    commit

--- Tabla.py
+++ Tabla.py
@@ -6,4 +6,8 @@ def tabla():
         if((numero1<9 or numero2<9) or (numero1>0 or numero2>0)):
             print('mal bobi es entre 1 y 9')
 
+        else:
+            for i in range(numero1):
+                for i in range(numero2):
+                    print(" * ", end='')
 
