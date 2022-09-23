f42142155db9b295dc855fcb2dc799018b7b9d3f
tereesaalvarez <alvdeporteresa@gmail.com>
2022-09-23 17:35:36 +0200

    commit

--- Tabla.py
+++ Tabla.py
@@ -1,3 +1,9 @@
+import sys
 def tabla():
-    tabla= int(input('Introduce el numero de filas y luego el de columnas'))
-    if tabla
+    if len(sys.argv)==3:
+        numero1= int(sys.argv[1])
+        numero2= int(sys.argv[2])
+        if((numero1<9 or numero2<9) or (numero1>0 or numero2>0)):
+            print('mal bobi es entre 1 y 9')
+
+
